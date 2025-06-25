28e8675f5893e025ad6ce50a8cbab7a9441d5539
Tejas Jiddewar <145433955+tejas-0905@users.noreply.github.com>
2025-06-25 15:21:26 +0530

    add event listener for Celsius input

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,4 @@ const cel = document.getElementById("cel");
 const fah = document.getElementById("fah");
 const kel = document.getElementById("kel");
 cel.addEventListener("input", () => {
+let c = parseFloat(cel.value);
