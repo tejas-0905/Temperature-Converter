3b115335c760f988f9630b69968302de8d266581
Tejas Jiddewar <145433955+tejas-0905@users.noreply.github.com>
2025-06-25 15:30:43 +0530

    apply 3D rotation to container based on mouse position

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,5 @@ const container = document.getElementById("container");
 document.addEventListener("mousemove", (e) => {
 const x = (window.innerWidth / 2 - e.pageX) / 25;
 const y = (window.innerHeight / 2 - e.pageY) / 25;
+ container.style.transform = `rotateY(${-x}deg) rotateX(${y}deg)`;
+});
