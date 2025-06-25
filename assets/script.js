167b2b6b58865e7560c455b37299ac0ca0b6b297
Tejas Jiddewar <145433955+tejas-0905@users.noreply.github.com>
2025-06-25 15:30:15 +0530

    calculate vertical offset for 3D tilt effect

--- assets/script.js
+++ assets/script.js
@@ -24,3 +24,4 @@ modeToggle.addEventListener("change", () => {
 const container = document.getElementById("container");
 document.addEventListener("mousemove", (e) => {
 const x = (window.innerWidth / 2 - e.pageX) / 25;
+const y = (window.innerHeight / 2 - e.pageY) / 25;
